d3d8b14d7aca2e47cf4010daa51f2f66e558c042
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2026-05-20 15:06:49 +0200

    Add RSA-3072 keypair generation, encryption, and decryption functions

--- include/secp256r1_rsakeys.h
+++ include/secp256r1_rsakeys.h
@@ -5,6 +5,8 @@
 #ifndef SCHNORR_THRESHOLD_DSA_ECDSA_SECP256R1_RSAKEYS_H
 #define SCHNORR_THRESHOLD_DSA_ECDSA_SECP256R1_RSAKEYS_H
 
+#include <stddef.h>
+
 #ifndef SECP256R1_ARG_NONNULL
 #if defined(__GNUC__) || defined(__clang__)
 #  define SECP256R1_ARG_NONNULL(x) __attribute__ ((__nonnull__(x)))
@@ -13,125 +15,82 @@
 #endif
 #endif
 
-#define RSA3072_PUBLIC_KEY_SIZE   (384)  // Modulo size
-#define RSA3072_CIPHERTEXT_SIZE   (384)  // RSA 3072-bit output
-#define RSA3072_PRIVATE_KEY_SIZE  (1536)  //
-#define RSA3072_PLAINTEXT_SIZE     (32)  // Max plaintext (256 bits)
+#define RSA3072_CIPHERTEXT_SIZE   (384)  // Output 3072-bit (384 byte)
+#define RSA3072_PLAINTEXT_SIZE     (32)  // Max plaintext (256 bit / 32 byte)
+#define RSA3072_PUBLIC_KEY_SIZE   (400) // Valore pulito, sicuro e ottimizzato
 
-typedef struct rsa_public_key {
-    unsigned char data[RSA3072_PUBLIC_KEY_SIZE];
-} rsa_public_key;
+typedef struct evp_pkey_st EVP_PKEY;
 
-typedef struct rsa_private_key {
-    unsigned char data[RSA3072_PRIVATE_KEY_SIZE];
-} rsa_private_key;
-
-/** Opaque data structure that holds an RSA encryption key-pair consisting of a public and a private key.
+/** Generate a native OpenSSL RSA-3072 keypair.
  *
- * The public exponent is fixed to 65537 in the current implementation, and the modulus is 3072 bits long.
- * The private key is stored in a format suitable for use with the RSA encryption and decryption functions provided by the library.
- */
-typedef struct rsa_keypair{
-    rsa_public_key public_key;
-    rsa_private_key private_key;
-} rsa_keypair;
-
-/** Generate a keypair for RSA encryption.
+ * Returns: 1 if the keypair was successfully generated. 0 on failure.
+ * Out:
+ * pubkey: pointer to an EVP_PKEY* that will hold the generated public key.
+ * privkey: pointer to an EVP_PKEY* that will hold the generated private key.
  *
- *  Returns: 1 if the keypair was successfully generated. 0 will cause commitment to fail.
- *  Args:
- *  Out: keypair: pointer to a keypair object to be filled with the generated keypair.
- *  In: data: arbitrary data pointer that is passed through to the nonce generation function.
- *
- *  The generated RSA keypair will have a fixed public exponent of 65537 and a modulus of 3072 bits.
- *  The private key will be stored in a format suitable for use with the RSA encryption and decryption functions provided by the library.
  */
 int secp256r1_rsa_keypair_generate(
-        rsa_keypair *keypair,
-        void *data
-) SECP256R1_ARG_NONNULL(1);
-
-/** Get the public key from an RSA keypair.
- *
- *  Returns: 1 always.
- *  Args:
- *  Out: pubkey: pointer to an rsa_public_key object, set to the keypair public key.
- *  In: keypair: pointer to a keypair.
- */
-int secp256r1_rsa_keypair_pubkey_get(
-        rsa_public_key *pubkey,
-        const rsa_keypair *keypair
+        EVP_PKEY **pubkey,
+        EVP_PKEY **privkey
 ) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2);
 
-/** Get the private key from an RSA keypair.
+/** Encrypt a plaintext message using a native RSA public key.
  *
- *  Returns: 1 always.
- *  Args:
- *  Out: privkey: pointer to an rsa_private_key object, set to the keypair private key.
- *  In: keypair: pointer to a keypair.
- */
-int secp256r1_rsa_keypair_privkey_get(
-        rsa_private_key *privkey,
-        const rsa_keypair *keypair
-) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2);
-
-/** Free the memory associated with an RSA keypair.
- *
- *  Returns: 1 always.
- *  Args:
- *  In: keypair: pointer to a keypair to be freed. After this call, the keypair should not be used again.
- */
-int secp256r1_rsa_keypair_free(
-        rsa_keypair *keypair
-) SECP256R1_ARG_NONNULL(1);
-
-/** Encrypt a plaintext message using an RSA public key.
- *
- *  Returns: 1 if the encryption was successful. 0 will cause commitment to fail.
- *  Args:
- *  Out: ciphertext: pointer to a buffer of size RSA3072_CIPHERTEXT_SIZE to be filled with the resulting ciphertext.
- *  In: pubkey: pointer to an rsa_public_key object containing the public key to encrypt with.
- *      plaintext: pointer to a buffer containing the plaintext message to be encrypted. The plaintext must be exactly RSA3072_PLAINTEXT_SIZE bytes long (32 bytes).
+ * Returns: 1 if encryption was successful, 0 on failure.
+ * Out: ciphertext: buffer of size RSA3072_CIPHERTEXT_SIZE filled with the result.
+ * In:  pubkey: native OpenSSL public key.
+ * plaintext: buffer containing the message (must be exactly RSA3072_PLAINTEXT_SIZE bytes).
+ * out_len: pointer to size_t set to the length of the ciphertext (will be RSA3072_CIPHERTEXT_SIZE).
  */
 int secp256r1_rsa_encrypt(
         unsigned char *ciphertext,
-        const rsa_public_key *pubkey,
-        const unsigned char *plaintext
-) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2) SECP256R1_ARG_NONNULL(3);
+        EVP_PKEY *pubkey,
+        const unsigned char *plaintext,
+        size_t *out_len
+) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2) SECP256R1_ARG_NONNULL(3) SECP256R1_ARG_NONNULL(4);
 
-/** Decrypt a ciphertext message using an RSA private key.
+/** Decrypt a ciphertext message using a native RSA private key.
  *
- *  Returns: 1 if the decryption was successful. 0 will cause recovery to fail.
- *  Args:
- *  Out: plaintext: pointer to a buffer of size RSA3072_PLAINTEXT_SIZE to be filled with the resulting plaintext message.
- *  In: privkey: pointer to an rsa_private_key object containing the private key to decrypt with.
- *      ciphertext: pointer to a buffer containing the ciphertext message to be decrypted. The ciphertext must be exactly RSA3072_CIPHERTEXT_SIZE bytes long (384 bytes).
+ * Returns: 1 if decryption was successful, 0 on failure.
+ * Out: plaintext: buffer of size RSA3072_PLAINTEXT_SIZE filled with the result.
+ * In:  privkey: native OpenSSL private key.
+ * ciphertext: buffer containing the message (must be exactly RSA3072_CIPHERTEXT_SIZE bytes).
  */
 int secp256r1_rsa_decrypt(
         unsigned char *plaintext,
-        const rsa_private_key *privkey,
+        EVP_PKEY *privkey,
         const unsigned char *ciphertext
 ) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2) SECP256R1_ARG_NONNULL(3);
 
-/** Serialize an RSA public key to bytes for transmission.
+/** Free the memory associated with a native RSA key (public or private).
+ *
+ * Returns: 1 always.
+ * In: pkey: native OpenSSL key to be freed. After this call, the pointer should not be used.
+ */
+int secp256r1_rsa_key_free(
+        EVP_PKEY *pkey
+);
+
+/** Serialize an RSA public key to a binary DER buffer for transmission.
  *
  * Returns: 1 on success, 0 on failure.
- * Out:     output: pointer to output buffer (RSA3072_PUBLIC_KEY_SIZE bytes)
- * In:      keypair: pointer to the keypair containing the public key
+ * Out:     output: pointer to the destination buffer (must be at least RSA3072_PUBLIC_KEY_SIZE bytes).
+ * In:      pubkey: native OpenSSL public key to be serialized.
  */
 int secp256r1_rsa_public_key_serialize(
         unsigned char *output,
-        const rsa_keypair *keypair
+        EVP_PKEY *pubkey
 ) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2);
 
-/** Parse an RSA public key from bytes.
+/** Parse an RSA public key from a binary DER buffer.
  *
  * Returns: 1 on success, 0 on failure.
- * Out:     pubkey: pointer to a public key structure.
- * In:      input: pointer to serialized public key (RSA3072_PUBLIC_KEY_SIZE bytes)
+ * Out:     pubkey_out: pointer to an EVP_PKEY* variable that will hold the parsed public key.
+ * In:      input: pointer to the serialized public key buffer (RSA3072_PUBLIC_KEY_SIZE bytes).
+ * Note:    The parsed key must be freed by the caller using secp256r1_rsa_key_free().
  */
 int secp256r1_rsa_public_key_parse(
-        rsa_public_key *pubkey,
+        EVP_PKEY **pubkey_out,
         const unsigned char *input
 ) SECP256R1_ARG_NONNULL(1) SECP256R1_ARG_NONNULL(2);
 
---
+++ src/secp256r1_rsakeys.c
@@ -0,0 +1,143 @@
+#include "secp256r1_rsakeys.h"
+#include <string.h>
+
+#include <openssl/evp.h>
+#include <openssl/rsa.h>
+
+int secp256r1_rsa_keypair_generate(EVP_PKEY **pubkey, EVP_PKEY **privkey) {
+    if (!pubkey || !privkey) return 0;
+
+    EVP_PKEY_CTX *ctx = NULL;
+    EVP_PKEY *pkey_pair = NULL;
+    EVP_PKEY *pub_only = NULL;
+    int success = 0;
+
+    *pubkey = NULL;
+    *privkey = NULL;
+
+    ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    if (!ctx) goto cleanup;
+
+    if (EVP_PKEY_keygen_init(ctx) <= 0) goto cleanup;
+    if (EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 3072) <= 0) goto cleanup;
+    if (EVP_PKEY_keygen(ctx, &pkey_pair) <= 0) goto cleanup;
+
+    unsigned char temp_der[RSA3072_PUBLIC_KEY_SIZE];
+    unsigned char *p_write = temp_der;
+    int len = i2d_PublicKey(pkey_pair, &p_write);
+    if (len <= 0 || len > RSA3072_PUBLIC_KEY_SIZE) goto cleanup;
+
+    const unsigned char *p_read = temp_der;
+    pub_only = d2i_PublicKey(EVP_PKEY_RSA, NULL, &p_read, len);
+    if (!pub_only) goto cleanup;
+
+    *pubkey = pub_only;
+    *privkey = pkey_pair;
+
+    success = 1;
+
+    cleanup:
+    if (ctx) EVP_PKEY_CTX_free(ctx);
+    if (!success) {
+        if (pkey_pair) EVP_PKEY_free(pkey_pair);
+        if (pub_only) EVP_PKEY_free(pub_only);
+    }
+    return success;
+}
+
+int secp256r1_rsa_encrypt(
+        unsigned char *ciphertext,
+        EVP_PKEY *pubkey,
+        const unsigned char *plaintext,
+        size_t *out_len
+) {
+    if (!ciphertext || !pubkey || !plaintext || !out_len) return 0;
+
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(pubkey, NULL);
+    if (!ctx) return 0;
+
+    int success = 0;
+
+    if (EVP_PKEY_encrypt_init(ctx) <= 0) goto err;
+    if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING) <= 0) goto err;
+    if (EVP_PKEY_CTX_set_rsa_oaep_md(ctx, EVP_sha256()) <= 0) goto err;
+    if (EVP_PKEY_CTX_set_rsa_mgf1_md(ctx, EVP_sha256()) <= 0) goto err;
+
+    if (EVP_PKEY_encrypt(ctx, ciphertext, out_len, plaintext, RSA3072_PLAINTEXT_SIZE) <= 0) goto err;
+
+    success = 1;
+
+    err:
+    EVP_PKEY_CTX_free(ctx);
+    return success;
+}
+
+int secp256r1_rsa_decrypt(
+        unsigned char *plaintext,
+        EVP_PKEY *privkey,
+        const unsigned char *ciphertext
+) {
+    if (!plaintext || !privkey || !ciphertext) return 0;
+
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new(privkey, NULL);
+    if (!ctx) return 0;
+
+    int success = 0;
+
+    unsigned char temp_out[RSA3072_CIPHERTEXT_SIZE];
+    size_t out_len = sizeof(temp_out);
+
+    if (EVP_PKEY_decrypt_init(ctx) <= 0) goto err;
+    if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_OAEP_PADDING) <= 0) goto err;
+    if (EVP_PKEY_CTX_set_rsa_oaep_md(ctx, EVP_sha256()) <= 0) goto err;
+    if (EVP_PKEY_CTX_set_rsa_mgf1_md(ctx, EVP_sha256()) <= 0) goto err;
+    if (EVP_PKEY_decrypt(ctx, temp_out, &out_len, ciphertext, RSA3072_CIPHERTEXT_SIZE) <= 0) goto err;
+    if (out_len != RSA3072_PLAINTEXT_SIZE) goto err;
+
+    memcpy(plaintext, temp_out, RSA3072_PLAINTEXT_SIZE);
+
+    success = 1;
+
+    err:
+    EVP_PKEY_CTX_free(ctx);
+    return success;
+}
+
+int secp256r1_rsa_key_free(EVP_PKEY *pkey) {
+    if (pkey) {
+        EVP_PKEY_free(pkey);
+    }
+    return 1;
+}
+
+int secp256r1_rsa_public_key_serialize(unsigned char *output, EVP_PKEY *pubkey) {
+    if (!output || !pubkey) return 0;
+
+    memset(output, 0, RSA3072_PUBLIC_KEY_SIZE);
+
+    unsigned char *p_der = output;
+    int len = i2d_PublicKey(pubkey, &p_der);
+
+    if (len <= 0 || len > RSA3072_PUBLIC_KEY_SIZE) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int secp256r1_rsa_public_key_parse(EVP_PKEY **pubkey_out, const unsigned char *input) {
+    if (!pubkey_out || !input) return 0;
+
+    *pubkey_out = NULL;
+
+    const unsigned char *p_der = input;
+
+    EVP_PKEY *pkey = d2i_PublicKey(EVP_PKEY_RSA, NULL, &p_der, RSA3072_PUBLIC_KEY_SIZE);
+
+    if (!pkey) {
+        return 0;
+    }
+
+    *pubkey_out = pkey;
+    return 1;
+}
